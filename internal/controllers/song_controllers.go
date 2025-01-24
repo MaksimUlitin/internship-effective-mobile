5f771c0e886628a885529f84f854381a8b70904e
MaksimUlitin <maks_d030p_ulitin@mail.ru>
2025-01-24 14:56:09 +0300

    created the skeleton of the project

--- internal/controllers/song_controllers.go
+++ internal/controllers/song_controllers.go
@@ -1,1 +1,23 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func AddSongInfo(c *gin.Context) {
+
+}
+
+func GetSongs(c *gin.Context) {
+
+}
+
+func GetSongText(c *gin.Context) {
+
+}
+
+func UpdateSong(c *gin.Context) {
+
+}
+
+func DeleteSong(c *gin.Context) {
+
+}
--- internal/routes/user_routes.go
+++ internal/routes/user_routes.go
@@ -1,1 +1,17 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"internship-effective-mobile/internal/controllers"
+)
+
+func Router() *gin.Engine {
+	r := gin.Default()
+	r.POST("/info", controllers.AddSongInfo)
+	r.GET("/songs", controllers.GetSongs)
+	r.GET("/songs/:id/text", controllers.GetSongText)
+	r.PUT("/songs/:id", controllers.UpdateSong)
+	r.DELETE("/songs/:id", controllers.DeleteSong)
+
+	return r
+}
